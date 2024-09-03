086737a2392fe56150cd26e5cd5a046b8f240fea
harsh_vardhan <harsh10112001@gmail.com>
2024-09-04 00:30:54 +0530

    connecting to DB

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import connectDB from "./db/dbconnection.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+/*
+import express from "express";
+const app = express();
+
+;( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        app.on("error", (error)=>{
+            console.log("ERR : ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listning on ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})()
+
+*/
