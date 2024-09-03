b3f617bd3887f5dbc05f3fd410c4058d0d2dc35a
harsh_vardhan <harsh10112001@gmail.com>
2024-09-04 00:45:53 +0530

    adding then catch in index.js

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+import express from "express"
+
+const app = express()
+
 const connectDB = async () =>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 
         console.log(`MongoDB connected !! DB host: ${connectionInstance.connection.host}`)
+
+        app.on("error",(error)=>{
+            console.log("Error occured : ", error);
+            throw error
+        })
 
     } catch (error) {
         console.log("MONGODB connection failed : ", error);
